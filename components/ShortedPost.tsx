749c87ff1aeaea380e75402a39796e8b32660e66
Tahsin Özgür KOÇ <tahsinkoc6@gmail.com>
2023-11-21 03:16:18 +0300

    Adding date into posts

--- components/ShortedPost.tsx
+++ components/ShortedPost.tsx
@@ -9,17 +9,22 @@ type Props = {
         id: number,
         image: string,
         title: string,
-        content: string
+        content: string,
+        createdAt: string
     },
     mod: number
 }
 
 function ShortedPost({ item, mod }: Props) {
-
+    const JsDate = new Date(item.createdAt)
+    const lastDate = JsDate.toLocaleDateString()
     const contentShorted = item.content.slice(0, 550)
     const set = [contentShorted, item.content]
     const btns = [<Link href={`post/${item.id}`}>
-        <button className='bg-pink-800 text-white px-8 py-1 mb-4 rounded'>Read More</button>
+        <div className='w-full flex items-center justify-between'>
+            <button className='bg-pink-800 text-white px-8 py-1 mb-4 rounded'>Read More</button>
+            <p className='text-pink-800'>{lastDate}</p>
+        </div>
     </Link>, <></>]
     return (
         <div className='w-11/12 border-b-2 border-pink-950 my-8 pt-4 px-4'>
